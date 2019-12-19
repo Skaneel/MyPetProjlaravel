b08d14b88f1e62d4b39ecc4e20d7083a0764120c
aleksandr <aleksandr1104nk@gmail.com>
2019-12-19 22:25:23 +0200

    3 С

---
+++ MylaravelProj/resources/views/userview.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>From</title>
+</head>
+<body>
+<form action="" method="POST"><input type="text" nama="user">
+  <br> <br>
+  <input type="text" nama="email">
+  <br><br>
+  <button type="submit">Submit</button>
+  </form>
+</body>
+</html>
--- MylaravelProj/routes/web.php
+++ MylaravelProj/routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//test
+Route::view('/userview', 'userview');
